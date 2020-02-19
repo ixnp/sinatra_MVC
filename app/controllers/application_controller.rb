a87eab63de75e9c6f633d16ec0fa88ac9a714fa5
ixnp <enprocopios@gmail.com>
2020-02-19 14:14:18 -0800

    added patch and form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,48 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get "/" do
     erb :welcome
   end
+
+  get '/dishes' do
+    @dishes = Dish.all
+    erb :index
+  end
+
+  get '/dishes/new' do
+    erb :new
+  end 
+
+
+  get '/dishes/:id' do
+    @dish = Dish.find(params[:id])
+    erb :show
+  end
+
+  get '/dishes/:id/edit' do
+    @dish = Dish.find(params[:id])
+    erb :edit
+  end 
+
+  post '/dishes' do
+    
+    dish = Dish.create(name:params[:name],description: params[:description], price: params[:price])
+    redirect "/dishes/#{dish.id}"
+  end
+
+  patch "/dishes/:id" do
+    dish = Dish.find(params[:id])
+    params.delete(:"_method")
+    dish = dish.update(params)
+    redirect "/dishes/#{params[:id]}"
+
+  end 
+
+
+
 
 end
--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,23 +1,3 @@
 class DishesController < ApplicationController
-  #index
-  get '/dishes' do
-    @dishes = Dish.all
-    erb :index
-  end
-
-  get '/dishes/:id' do
-    @dish = Dish.find(params[:id])
-    erb :show
-  end
-
-  post '/dishes' do
-    dish = Dish.create(name:params[:name],description: params[:description], price: params[:price])
-  end
-
-  delete '/dishes/:id' do
-  Dish.destroy(params[:id])
-  byebug
-
-  end
-
+  
 end
